e95c353dc04e0d6b43fa0f97d9da310e2c27bc64
AyxanNabatov <107032159+AyxanNabatov@users.noreply.github.com>
2022-06-30 12:18:05 +0400

    Add files via upload

---
+++ obstraction/Obstraction/Obstraction/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obstraction
+{
+   public class Dog
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("Dog sound");
+
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Dog eating");
+        }
+    }
+}
---
+++ obstraction/Obstraction/Obstraction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Obstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Sound();
+            dog.Eat();
+
+            Bird bird = new Bird();
+            bird.Sound();
+            bird.Eat();
+
+        }
+    }
+}
